5551c0b96c57f7daec1878dfaae4b23956dd60c5
Long Duong <long66704@gmail.com>
2024-05-29 20:40:21 +0700

    mock data

---
+++ src/Models/mock_data.py
@@ -0,0 +1,5 @@
+data = [
+  {'name': 'Dương Phước Long', 'std_id': '102190024', 'class': '19TCLC_DT1', 'address': 'Hiền Lương, Thừa Thiên Huế', 'image': 'images/102190024.jpg'},
+  {'name': 'Nguyễn Hữu Phúc', 'std_id': '102210225', 'class': '21TCLC_DT3', 'address': 'Hiền Lương, Thừa Thiên Huế', 'image': 'images/Phuc.jpg'},
+  {'name': 'Lê Văn Bảo Quốc', 'std_id': '102210227', 'class': '21TCLC_DT3', 'address': 'Hiền Lương, Thừa Thiên Huế', 'image': 'images/Quoc.jpg'}
+]
